678366a4243a84fb5ae780f7d713f4eaace1e979
Antonio Jiménez <marshday5@gmail.com>
2023-11-28 00:05:00 -0600

    first component with react and making the diference between component and element, and understanding the props on my own components

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <TodoItem text = {3}/>
+      <TodoItem text = {"Hola demonio"}></TodoItem>
+      <TodoItem text = {"Este es un prop que es usado para darle más dinamismo a mis componentes en react al parecer xd, es una propiedad de mi componente"}></TodoItem>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hola hijos de perra desde react <code>src/App.js</code> y guarda para recargar.
-        </p>
+        </p> 
         <a
           className="App-link"
           href="https://platzi.com/reactjs"
@@ -21,5 +25,16 @@ function App() {
     </div>
   );
 }
+// className me indica que es xml
 
+function TodoItem(props) {
+  return (
+    <li className='daddy'>
+      <span>V</span>
+      <p className='aver'>{props.text}</p>
+      <span>X</span>
+    </li>
+  );
+}
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -5,3 +5,6 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+// createRoot(document.getElementById('root')) de ReactDOM, se utiliza para crear una raíz que mostrará los componentes de React dentro de un elemento html o nodo DOM del navegador. Después de esto, nace .render(<App />) y se encargará de crear en el navegador, lo contenido en <App />. ¿De qué archivo viene <App />? Del App.js de la carpeta src.
+
